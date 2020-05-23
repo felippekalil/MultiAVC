a27fb0777d23b7238766fd6726bc1e77cc1445f4
felippekalil <35376103+felippekalil@users.noreply.github.com>
2020-05-22 22:02:10 -0300

    Retirada de comentários

--- MotoAVC/MotoAVC.ino
+++ MotoAVC/MotoAVC.ino
@@ -5,7 +5,6 @@
 */
 #include "EEPROM.h"
 #include "Controle.h"
-//#include "Interface.h"/*
 #include "IHMv1.h"
 using namespace IHMv1;
 
@@ -14,8 +13,7 @@ Linha linhas[] = { Linha("   In:", &Controle::valorTensaoDoArco, 1, 5, "V"),
 					Linha("  Out:", &Controle::valorSaidaCorrente, 0, 5, "A") };
 
 VarFloat referencia = { &Controle::referencia , 0.1, 99.9, 0.1 };
-//Ihm ihm(&referencia, &Controle::valorTensaoDoArco);/*
-Ihm ihm(&referencia, linhas, 3);//*/
+Ihm ihm(&referencia, linhas, 3);
 float refEeprom = 0;
 
 
@@ -23,7 +21,6 @@ void setup() {
     Serial.begin(9600);
     Serial.println("Inicializado!");
     Controle::setupControle();
-   // Interface::setupInterface(&Controle::referencia, &Controle::valorTensaoDoArco);    /*
     ihm.setup();//*/
     EEPROM.get(0, Controle::referencia);
     refEeprom = Controle::referencia;
@@ -31,7 +28,7 @@ void setup() {
     Serial.println("Ref: " + String(Controle::referencia, 1) + " V");
 }
 
-void atualizaEeprom() // se n„o for lenta a leitura da eeprom, d· pra rodar o update diretamente, e deletar o refEeprom
+void atualizaEeprom() // se n√£o for lenta a leitura da eeprom, d√° pra rodar o update diretamente, e deletar o refEeprom
 {
     if(refEeprom != Controle::referencia)
     {
@@ -45,8 +42,7 @@ void atualizaEeprom() // se n„o for lenta a leitura da eeprom, d· pra rodar o up
 void loop() {
     Controle::atua();
 	if(millis() % 300 == 0)
-        //Interface::imprimeInterface();/*
-        ihm.imprimeInterface();//*/
+        ihm.imprimeInterface();
     if(ihm.varAjustadas())
         atualizaEeprom();
 }
